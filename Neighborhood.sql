a8861dc2c7e1133746c6aa367b5111ae691ea9da
William Wang <swang45@uw.edu>
2022-03-11 18:03:40 -0800

    Added Neighborhood Code

--- Neighborhood.sql
+++ Neighborhood.sql
@@ -1,3 +1,23 @@
+CREATE PROCEDURE ww_Insert_Neighborhood
+@N_Name varchar(30)
+AS
+
+BEGIN TRANSACTION T1
+INSERT INTO tblNEIGHBORHOOD (NeighborhoodName)
+VALUES (@N_Name)
+GO
+
+INSERT INTO tblNEIGHBORHOOD (NeighborhoodName)
+VALUES
+('U District'),
+('Roosevelt'),
+('Northlake'),
+('Green Lake'),
+('Capitol Hill'),
+('Pike-Market'),
+('Eastlake')
+GO
+
 Exec dbo.ww_Insert_Neighborhood
   "Ada"
 Go
