2282bc0db4863e3b5fdd244f32631084dda04002
fretws <fretws@uw.edu>
2022-03-11 19:51:39 -0800

    Fix typos and mistakes

--- Complex_Queries.sql
+++ Complex_Queries.sql
@@ -1,33 +1,47 @@
 -- Shane Complex Queries
 
--- Write a query to find the average listing price and user age of properties rented or bought by users between 45 and 60 years of age, grouped by rent vs buy.
+-- Write a query to find the average listing price and user age of properties rented or bought by users between 20 and 30 years of age, grouped by rent vs buy.
 
-Select ET.EventTypeName, AVG(E.Price) AvgPrice, AVG(U.Calc_Age) AvgAge
+Select ET.EventTypeName, AVG(E.Price) AvgPrice, AVG(U.UserAge) AvgAge
 From tblEVENT E
   Join tblEVENT_TYPE ET On ET.EventTypeID = E.EventTypeID
   Join tblPROPERTY_USER PU On PU.PropertyUserID = E.PropertyUserID
   Join tblUSER U On U.UserID = PU.UserID
-Where U.Calc_Age Between 45 And 60
+Where U.UserAge Between 20 And 30
+  And (EventTypeName = 'Purchase property' Or EventTypeName = 'Rent Property')
 Group By ET.EventTypeID, ET.EventTypeName
 
 Select * From tblUser
 
--- Write the SQL query to find every purchase made by users who were born after 1978 and have made at least three purchases before the age of 40
+-- Write the SQL query to find every purchase made by users who were born after 1978 and have made at least two purchases
 
-Select qryA.PropertyID, qryA.PropAddress
-
-From tblPROPERTY P
-  Join tblPROPERTY_USER PU On PU.PropertyID = P.PropertyID
-  Join tblUSER U On U.UserID = PU.UserID
-  Join tblEVENT E On E.PropertyUserID = PU.PropertyUserID
-  Join tblEVENT_TYPE ET On ET.EventTypeID = E.EventTypeID
-Where U.BirthDate < '1978-01-01'
-  And ET.EventTypeName = 'Purchase property'
+Select qryA.PropertyID, qryA.PropAddress, qryB.Buyer, qryB.NumPurchases
+From (
+  Select P.PropertyID, P.PropAddress, U.UserID
+  From tblPROPERTY P
+    Join tblPROPERTY_USER PU On PU.PropertyID = P.PropertyID
+    Join tblUSER U On U.UserID = PU.UserID
+    Join tblEVENT E On E.PropertyUserID = PU.PropertyUserID
+    Join tblEVENT_TYPE ET On ET.EventTypeID = E.EventTypeID
+  Where U.BirthDate > '1978-01-01'
+    And ET.EventTypeName = 'Purchase property'
+) As qryA, (
+  Select U.UserID, U.FirstName + ' ' + U.LastName As Buyer, COUNT(P.PropertyID) As NumPurchases
+  From tblPROPERTY P
+    Join tblPROPERTY_USER PU On PU.PropertyID = P.PropertyID
+    Join tblUSER U On U.UserID = PU.UserID
+    Join tblEVENT E On E.PropertyUserID = PU.PropertyUserID
+    Join tblEVENT_TYPE ET On ET.EventTypeID = E.EventTypeID
+  Where ET.EventTypeName = 'Purchase property'
+  Group By U.UserID, U.FirstName, U.LastName
+  Having COUNT(P.PropertyID) > 1
+) As qryB
+Where qryA.UserID = qryB.UserID
 
 -- Anthony Complex Queries
 
 --Write the SQL query to find all properties that have both cooling and heating system built after 2015[Anthony]
-SELECT P.PropertyID, P.PropertyName, P.PropAddress
+SELECT P.PropertyID, P.PropAddress
 FROM tblPROPERTY P
     JOIN tblPROPERTY_AMENITY PA on P.PropertyID = PA.PropertyID
     JOIN tblAMENITY A on PA.AmenityID = A.AmenityID
@@ -35,12 +49,12 @@ FROM tblPROPERTY P
 WHERE A.AmenityName LIKE '%Cooling'
 AND A.AmenityName LIKE '%Heating'
 AND P.PropBuiltYear > '2015'
-GROUP BY P.PropertyID, P.PropertyName, P.PropAddress
+GROUP BY P.PropertyID, P.PropAddress
 GO--could use some help
 
 ---Write a query to find the neighborhood which has the lowest average age of users who purchase houses in that neighborhood.[Anthony]
 SELECT Top(1) N.NeighborhoodName
-FROM tbleNEIGHBORHOOD N
+FROM tblNEIGHBORHOOD N
     JOIN tblPROPERTY P on N.NeighborhoodID = P.NeighborhoodID
     JOIN tblPROPERTY_USER PU on P.PropertyID = PU.PropertyID
     JOIN tblUSER U on PU.UserID = U.UserID
